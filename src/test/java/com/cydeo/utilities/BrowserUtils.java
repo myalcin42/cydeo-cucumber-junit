4de0e98e3196ef830a6cd328167da71254e7e842
Munir Yalcin Saracoglu <myalcinsaracoglu42@gmail.com>
2023-02-09 14:14:13 +0300

    Initial commit

---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,63 @@
+package com.cydeo.utilities;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class BrowserUtils {
+
+        /*
+    this method will accept int(in seconds)  and execute Thread.sleep for given duration
+     */
+
+    public static void sleep(int second){
+        second *= 1000;
+
+        try{
+            Thread.sleep(second);
+        }catch(InterruptedException e){
+
+        }
+    }
+
+    //this method accepts three arguments
+    //Arg1: webdriver
+    //Arg2: expectedInUrl : for verify if the url contains given String
+    //If condition matches, will break loop
+    //Arg3: expectedInTitle to be compared against actualTitle
+    public static void switchWindowAndVerify( String expectedInUrl, String expectedInTitle ){
+
+        Set<String> allWindowHandles = Driver.getDriver().getWindowHandles();
+
+        for (String each : allWindowHandles) {
+            Driver.getDriver().switchTo().window(each);
+
+            System.out.println("current url: " + Driver.getDriver().getCurrentUrl());
+
+            if(Driver.getDriver().getCurrentUrl().contains(expectedInUrl)){
+                break;
+            }
+        }
+
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+    }
+
+    //this method accepts a String "expectedtitle" and Asserts if it is true
+    public static void verifyTitle(String expectedTitle){
+        Assert.assertEquals(Driver.getDriver().getTitle(), expectedTitle);
+    }
+
+    //creating a utility method for ExplicitWait so we dont have to repeat lines
+    public static void waitForInvisibilityOf(WebElement webElement){
+        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+    }
+}
---
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -0,0 +1,67 @@
+package com.cydeo.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    /*
+   creating a private constructor so we are closing access to the object ogf this class from outside the class
+    */
+    private Driver(){}
+
+    /*
+    we make WebDriver private, because we want to close access from the outside the class
+    we make it static because we will use it in a static method
+     */
+
+    private static WebDriver driver;
+    /*
+    create a re-usable utility method which will return same driver instance when we call it
+     */
+
+    public static WebDriver getDriver(){
+
+        if(driver == null){
+            /*
+            we read our browser type from configuration.properties
+            this way, we can control which browser is opened from outside our code, from configuration.properties
+             */
+            String browserType = ConfigurationReader.getProperty("browser");
+            /*
+            depending on the browserType that will be return from configuration.properties file switch statement will determine the case, and open the matching browser
+             */
+
+            switch(browserType){
+
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+            }
+        }
+
+        return driver;
+    }
+
+    //this method will make sure our driver value is always null after using quit() method
+    public static void closeDriver(){
+
+        if(driver != null){
+            driver.quit(); //this will terminate the existing session, value will not even be null
+            driver = null;
+        }
+    }
+}
