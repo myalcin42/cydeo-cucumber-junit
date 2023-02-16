94f861891ef5cdd7ce4322fae3c52a4ecc12ec86
Munir Yalcin Saracoglu <myalcinsaracoglu42@gmail.com>
2023-02-16 14:23:17 +0300

    Day15 cucumber

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -11,10 +11,22 @@ In the class we will be able to pass pre & post conditions to each scenario and 
 public class Hooks {
     //Hooks bütün feature dosyalarında bulunan senaryoları etkiler ve specify etmediğin sürece burada yazdığın Before ve After içerisinde bulunan ifadeler çalıştırılır Background ise sadece belirttiğin feature dosyası içerisinde gereklidir
     //import from io.cucumber.java not from junit
-    @Before
+    @Before(order = 0) //bu şekilde hangi Before önce çalışacak belirleyebiliriz
     public void setupScenario(){
 
         System.out.println("=====Setting up browser using cucumber @Before");
+    }
+
+    @Before (value = "@login", order = 1)//aynı anda 1 den fazla aynı annotation'dan oluşturulabilir Before yanına yazdığımız @login annotasyonunun aynısını login feature da yazdık o yüzden sadece o feature dosyasında çalışacak
+    public void setupScenarioForLogins(){
+
+        System.out.println("=====this will only apply to scenarios with @login tag");
+    }
+
+    @Before(value = "@db", order = -1)
+    public void setupForDatabaseScenarios(){
+
+        System.out.println("=====this will only apply to scenarios with @db tag");
     }
 
     @After
@@ -23,13 +35,13 @@ public class Hooks {
         System.out.println("========Scenario ended/ Take screenshot if failed!");
     }
 
-    @BeforeStep
+    @BeforeStep //her adımdan önce birkez çalıştırılır user enters librarian username mesela bu adımdan önce çalıştırılır sonraki adıma geçtiğinde tekrar çalıştırılır
     public void setupStep(){
 
         System.out.println("-------------> applying setup using @BeforeStep");
     }
 
-    @AfterStep
+    @AfterStep //her adımdan sonra birkez çalıştırılır user enters librarian username mesela bu adımdan önce çalıştırılır sonraki adıma geçtiğinde tekrar çalıştırılır
     public void afterStep(){
         System.out.println("------------> applying tearDown using @AfterStep");
     }
