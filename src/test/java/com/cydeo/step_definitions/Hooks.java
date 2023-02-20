8df6771b489176839f34811e3c578074d60d9e0e
Munir Yalcin Saracoglu <myalcinsaracoglu42@gmail.com>
2023-02-20 10:16:36 +0300

    Day16 cucumber

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -11,19 +11,19 @@ In the class we will be able to pass pre & post conditions to each scenario and 
 public class Hooks {
     //Hooks bütün feature dosyalarında bulunan senaryoları etkiler ve specify etmediğin sürece burada yazdığın Before ve After içerisinde bulunan ifadeler çalıştırılır Background ise sadece belirttiğin feature dosyası içerisinde gereklidir
     //import from io.cucumber.java not from junit
-    @Before(order = 0) //bu şekilde hangi Before önce çalışacak belirleyebiliriz
+    @Before(order = 1) //bu şekilde hangi Before önce çalışacak belirleyebiliriz
     public void setupScenario(){
 
         System.out.println("=====Setting up browser using cucumber @Before");
     }
 
-    @Before (value = "@login", order = 1)//aynı anda 1 den fazla aynı annotation'dan oluşturulabilir Before yanına yazdığımız @login annotasyonunun aynısını login feature da yazdık o yüzden sadece o feature dosyasında çalışacak
+    @Before (value = "@login", order = 2)//aynı anda 1 den fazla aynı annotation'dan oluşturulabilir Before yanına yazdığımız @login annotasyonunun aynısını login feature da yazdık o yüzden sadece o feature dosyasında çalışacak
     public void setupScenarioForLogins(){
 
         System.out.println("=====this will only apply to scenarios with @login tag");
     }
 
-    @Before(value = "@db", order = -1)
+    @Before(value = "@db", order = 0)
     public void setupForDatabaseScenarios(){
 
         System.out.println("=====this will only apply to scenarios with @db tag");
