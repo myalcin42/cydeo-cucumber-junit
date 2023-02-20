ac9619bd90bc5ed93da386c34f779d5165ce35bc
Munir Yalcin Saracoglu <myalcinsaracoglu42@gmail.com>
2023-02-20 11:41:57 +0300

    Day16 cucumber

--- src/test/java/com/cydeo/pages/GoogleSearchPage.java
+++ src/test/java/com/cydeo/pages/GoogleSearchPage.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class GoogleSearchPage {
@@ -10,5 +12,7 @@ public class GoogleSearchPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    @FindBy(name = "q")
+    public WebElement searchBox;
 
 }
--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,19 +1,30 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class GoogleStepDefinitions {
+
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
     @When("user types apple and clicks enter")
     public void user_types_apple_and_clicks_enter() {
 
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
     }
     @Then("user sees apple in the google title")
     public void user_sees_apple_in_the_google_title() {
 
+        String expectedTitle = "apple - Google Search";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        //Junit assertion accepts first arg as expected, second arg as actual but in TestNg it is opposite ve assertion fail olursa yazdıraqcağımız mesajı testng de sona yazarken junitde başa yazarız
+        Assert.assertEquals("Title is not as expected", expectedTitle, actualTitle);
+       // ya da Assert.assertTrue(actualTitle.equals(expectedTitle));
     }
 
     @When("user is on Google search page")
