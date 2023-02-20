a6750ba80142b39ccd15cf264ca509db2551f1bf
Munir Yalcin Saracoglu <myalcinsaracoglu42@gmail.com>
2023-02-20 13:10:48 +0300

    Day16 cucumber

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,10 +1,9 @@
 package com.cydeo.step_definitions;
 
 import com.cydeo.utilities.Driver;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
+import io.cucumber.java.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 /*
 In the class we will be able to pass pre & post conditions to each scenario and each step
@@ -31,7 +30,10 @@ public class Hooks {
     }
 
     @After
-    public void teardownScenario(){
+    public void teardownScenario(Scenario scenario){
+
+        byte[] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot,"image/png", scenario.getName());
 
         Driver.closeDriver();
 //        System.out.println("========Closing browser using cucumber @After");
